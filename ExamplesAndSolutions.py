12226c5832521fb9dc387e22ca3a763a07d137a7
Kamryn Ohly <kamohly@gmail.com>
2022-02-27 12:13:45 -0500

    thank you

--- ExamplesAndSolutions.py
+++ ExamplesAndSolutions.py
@@ -10,7 +10,7 @@ print(todos) # ['brush teeth', 'make bed', 'grab a coffee']
 # What do we do need to do first in the morning?
 print(todos[1]) 
 
-# What will this print? 
+# Nope!!! What will this print? 
 # It prints "make bed" !!!!
 
 # Arrays actually start counting with 0!
@@ -24,24 +24,28 @@ print(todos[2]) # grab a coffee
 # This means that our list does not have that item!
 # Out of index is a super common error, be cautious of it!
 
-todos = ["brush teeth", "make bed", "grab a coffee"]
-
+# How do we add to our array?
 # What if we need to also need to feed our cats in the morning?
 todos.append("Feed the cats")
 
-# Let's check!
+# Let's check if it worked!
 print(todos) 
 # ['brush teeth', 'make bed', 'grab a coffee', 'Feed the cats']
 
 # What if we complete a todo item?
+# How do we remove something from an array?
 todos.remove("Feed the cats")
 
+# Check
 print(todos) # ['brush teeth', 'make bed', 'grab a coffee']
 
-# Or remove at a certain index
+# Or delete at a certain index:
 del todos[0]
 
+# Check
 print(todos) # ['make bed', 'grab a coffee']
+
+
 
 # Resetting our array (or comment out the prior lines)
 todos = ["brush teeth", "make bed", "grab a coffee"]
@@ -54,6 +58,8 @@ todos.insert(2, "Feed the cats")
 # Check
 print(todos) # ['brush teeth', 'make bed', 'Feed the cats', 'grab a coffee']
 
+# Fun fact: Python allows us to combine data types
+# Although I do not recommend it!
 test = [1, "hello", 3.14]
 print(test)
 
@@ -77,3 +83,12 @@ def getCoffee (order):
 print(getCoffee("Vanilla Latte")) # Here's your Vanilla Latte
 
 # Challenge: What if you order multiple coffees?
+
+def getMultipleCoffees(listOfOrders):
+    listOfCompletedCoffees = []
+    for coffee in listOfOrders:
+        listOfCompletedCoffees.append(coffee + " is done!")
+    return listOfCompletedCoffees
+
+# Test the code: Pass in an array as your input!
+print (getMultipleCoffees(["Vanilla Latte", "Cold Brew", "Mocha"]))
---
+++ PracticeForYou.py
@@ -0,0 +1,43 @@
+# This coding language is Python!
+# How can we represent our to-dos on a computer?
+
+# Let's use ARRAYS:
+todos = ["brush teeth", "make bed", "grab a coffee"]
+
+# Make your own array!
+# TO-DO
+
+# Print your array
+# TO-DO
+
+# Check one of the elements of your list & print it!
+# TO-DO
+
+# What index do arrays start at?
+# TO-DO
+
+# Print another item of your array
+# TO-DO
+
+# Add an element to your array
+# TO-DO
+
+# Remove or delete an element from your array
+# TO-DO
+
+# Challenge: add an element at a specific point in your array!
+# TO-DO
+
+# FUNCTIONS!
+
+# Create your own function:
+# TO-DO
+
+# Call your function:
+# TO-DO
+
+# Challenge: Combine arrays & functions!
+# TO-DO
+
+# Test your challenge code!
+# TO-DO
