2004b01194f21ed3b74a39ae0271915f99a8acbe
Kamryn Ohly <kamohly@gmail.com>
2022-02-27 16:22:41 -0500

    done

--- PracticeForYou.py
+++ PracticeForYou.py
@@ -1,5 +1,5 @@
 # This coding language is Python!
-# How can we represent our to-dos on a computer?
+# How can we represent our todos on a computer?
 
 # Let's use ARRAYS:
 todos = ["brush teeth", "make bed", "grab a coffee"]
